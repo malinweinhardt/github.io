f4b2a965ef1f65719cd6669074e7596c16a6439b
malinweinhardt <malin.weinhardt@gmail.com>
2024-05-29 11:32:50 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var typed = new Typed('#element', {
+    strings: ["lost <span style=font-family:Elegant>art</span>"],
+    typeSpeed: 30,
+    showCursor: false,
+  });
+
+  var typed = new Typed('#element1', {
+    strings: ["of <span style=font-family:Elegant>loving</span>"],
+    typeSpeed: 30,
+    startDelay: 1000,
+    showCursor: false,
+
+  });
